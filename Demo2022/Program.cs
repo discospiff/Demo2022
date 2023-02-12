5c69324a37a325ed78ac8d1e23a2f1bf9cd78201
Brandan <brandan.jones@uc.edu>
2023-02-11 22:11:30 -0500

    Add Swagger doc.

--- Demo2022/Program.cs
+++ Demo2022/Program.cs
@@ -1,10 +1,31 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Speciemns API",
+        Description = "An ASP.NET Core Web API for managing Specimens",
+        TermsOfService = new Uri("https://plantplaces.com/privacy.shtml"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contact PlantPlaces",
+            Url = new Uri("https://plantplaces.com/perl/entercomments.pl")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Plant Places License",
+            Url = new Uri("https://plantplaces.com/privacy.shtml")
+        }
+    });
+});
 
 var app = builder.Build();
 
